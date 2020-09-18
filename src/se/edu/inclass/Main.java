245145bd1823ff72c8a1ad2b774d6ce8a26def92
Akshay <okkhoy@gmail.com>
2020-09-18 17:01:17 +0800

    Print data using streams in main

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -22,6 +22,11 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+
+        printDeadlinesUsingStreams(tasksData);
+
+        System.out.println("Total number of deadlines using streams: " + countDeadlinesUisngStreams(tasksData));
+
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
